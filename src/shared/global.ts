f5e361405e276a29143ba335bc33c66521a91bb5
Zhihao Cui <davidcui2@me.com>
2020-12-27 23:20:08 +0000

    Add Icon component (#10)
    * Add Icon component with material icon font
    * Update mdx heading

--- src/shared/global.ts
+++ src/shared/global.ts
@@ -1,6 +1,7 @@
 import { createGlobalStyle, css } from "styled-components";
 
 import GoldmanSansRegular from "./GoldmanSans_W_Rg.woff2";
+import MaterialIconsRegular from "./MaterialIcons-Regular.ttf";
 
 export const bodyStyles = css`
   font-family: "Goldman Sans Regular";
@@ -15,7 +16,41 @@ export const GlobalStyle = createGlobalStyle`
         font-style: normal;
     }
 
+    @font-face {
+      font-family: 'Material Icons';
+      font-style: normal;
+      font-weight: 400;
+      src: local('Material Icons'),
+        local('MaterialIcons-Regular'),
+        url(${MaterialIconsRegular}) format('truetype');
+    }
+
     body {
       ${bodyStyles}
+    }
+
+    .material-icons {
+      font-family: 'Material Icons';
+      font-weight: normal;
+      font-style: normal;
+      font-size: 24px;  /* Preferred icon size */
+      display: inline-block;
+      line-height: 1;
+      text-transform: none;
+      letter-spacing: normal;
+      word-wrap: normal;
+      white-space: nowrap;
+      direction: ltr;
+    
+      /* Support for all WebKit browsers. */
+      -webkit-font-smoothing: antialiased;
+      /* Support for Safari and Chrome. */
+      text-rendering: optimizeLegibility;
+    
+      /* Support for Firefox. */
+      -moz-osx-font-smoothing: grayscale;
+    
+      /* Support for IE. */
+      font-feature-settings: 'liga';
     }
 `;
